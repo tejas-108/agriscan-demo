9ed70abfe5894640ee122fcb94d0c3ee06969e29
tejas-108 <104578846+tejas-108@users.noreply.github.com>
2026-09-03 14:51:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+from ultralytics import YOLO
+
+st.title("AI Image Classifier")
+
+@st.cache_resource
+def load_model():
+    return YOLO("yolo11n.pt")
+
+model = load_model()
+
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+    
+    with st.spinner("Analyzing..."):
+        results = model(image)
+        res_plotted = results[0].plot()
+        
+    st.subheader("Results:")
+    st.image(res_plotted, caption="Identified Objects", use_container_width=True)
